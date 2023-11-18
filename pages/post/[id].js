8d737ea323e660fbb204f5c6674b2338e0ba9b1e
Dmytro Petrenko <petrenko_dmytro@ukr.net>
2023-11-18 17:23:41 +0200

    add db mockAPI, used getInitialProps

--- pages/post/[id].js
+++ pages/post/[id].js
@@ -1,11 +1,27 @@
 // переход на страницу с динамическим параметром
 
-import { useRouter } from "next/router";
+// import { useRouter } from "next/router";
+import MainLayout from "../../components/MainLayout";
+import Link from "next/link";
 
-const Post = () => {
-  const router = useRouter();
-//   console.log(router);
-  return <div>Post {router.query.id}</div>;
+const Post = ({ post }) => {
+  // const router = useRouter();
+  //   console.log(router);
+  return (
+    <MainLayout>
+      <h1>{post.title}</h1>
+      <p>{post.body}</p>
+      <Link href={"/posts"}>Back to all posts</Link>
+    </MainLayout>
+  );
+};
+
+Post.getInitialProps = async (ctx) => {
+  console.log(ctx.query);
+  const res = await fetch(`http://localhost:4200/posts/${ctx.query.id}`);
+  const post = await res.json();
+
+  return { post };
 };
 
 export default Post;
--- pages/posts.js
+++ pages/posts.js
@@ -1,7 +1,20 @@
+import { useState, useEffect } from "react";
 import Router from "next/router";
 import MainLayout from "../components/MainLayout";
+import Link from "next/link";
 
-const Posts = () => {
+const Posts = ({ posts }) => {
+  // const [posts, setPosts] = useState([]);
+  // useEffect - в рамках SSR бесполезен
+  // useEffect(() => {
+  //   async function load() {
+  //     const res = await fetch("http://localhost:4200/posts");
+  //     const json = await res.json();
+  //     setPosts(json);
+  //   }
+  //   load();
+  // }, []);
+
   const linkClickHandler = () => {
     // Router.push() - go to url
     Router.push("/");
@@ -9,9 +22,23 @@ const Posts = () => {
   return (
     <MainLayout title={"Posts Page"}>
       <h1>Posts</h1>
+      <ul>
+        {posts.map((post) => (
+          <li key={post.id}>
+            <Link href={`/post/${post.id}`}>{post.title}</Link>
+          </li>
+        ))}
+      </ul>
       <button onClick={linkClickHandler}>Go back to Home</button>
     </MainLayout>
   );
+};
+
+// getInitialProps - стфтический метод который выполняется на сервере; теперь поисковики могут видеть контент
+Posts.getInitialProps = async () => {
+  const res = await fetch("http://localhost:4200/posts");
+  const posts = await res.json();
+  return { posts }; // важно вернуть обект
 };
 
 export default Posts;
